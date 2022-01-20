f8397142c1882e30b97971b434cf6c7e1208eadf
MuhtasimTanmoy <taaanmo0oy@gmail.com>
2022-01-20 11:06:12 +0600

    Initial Commit

---
+++ WebRTC arm64/WebRTC_arm64.h
@@ -0,0 +1,18 @@
+//
+//  WebRTC_arm64.h
+//  WebRTC arm64
+//
+//  Created by tanmoy on 20/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebRTC_arm64.
+FOUNDATION_EXPORT double WebRTC_arm64VersionNumber;
+
+//! Project version string for WebRTC_arm64.
+FOUNDATION_EXPORT const unsigned char WebRTC_arm64VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebRTC_arm64/PublicHeader.h>
+
+
